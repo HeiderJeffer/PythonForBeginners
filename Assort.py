825e17b31efef36ff04d06c39268f3fa374dcd45
Heider Jeffer <hheider.jeffer@gmail.com>
2020-08-19 22:02:47 +0200

    ascending and descending order

--- Assort.py
+++ Assort.py
@@ -2,6 +2,6 @@
 
 num = [10,2,3,56,1,11,100.23,765,9,10,13,9,12,33,45,87]
 # num.sort()  # in ascending order
-num.sort(reverse=False)  # True= descending order. and False= ascending order
+num.sort(reverse=True)  # True= descending order. and False= ascending order
 print(num)
 #  arrange the number in descending order
